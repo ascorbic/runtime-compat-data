0352ca9b1db3d5d938b3460867b0f36460cd8cd1
ascorbic <ascorbic@gmail.com>
2024-03-10 23:11:09 +0000

    Process data

--- scripts/process-test-results.js
+++ scripts/process-test-results.js
@@ -1,1 +1,45 @@
-import data from "@mdn/browser-compat-data";
+// @ts-check
+import { readdir, writeFile } from "node:fs/promises";
+import data from "@mdn/browser-compat-data" assert { type: "json" };
+import { deepCreate, get } from "./traverse-utils.js";
+const runtimeDir = new URL("../src/runtimes", import.meta.url);
+
+const keys = await readdir(runtimeDir);
+/** @type {Record<string, Set<string>>} */
+const support = {};
+for (const key of keys) {
+  const data = await import(`../src/runtimes/${key}/data.json`, {
+    assert: { type: "json" },
+  });
+  support[key] = new Set(data.default);
+}
+const union = new Set(
+  Object.values(support)
+    .flatMap((set) => [...set])
+    .sort(),
+);
+
+const compat = {};
+
+for (const feature of union) {
+  const entry = deepCreate(compat, feature);
+  if (!entry.__compat) {
+    const browserCompat = get(data, `${feature}.__compat`) ?? {};
+    entry.__compat = {
+      ...browserCompat,
+      support: Object.fromEntries(
+        keys.map((key) => [
+          key,
+          {
+            version_added: support[key].has(feature),
+          },
+        ]),
+      ),
+    };
+  }
+}
+
+await writeFile(
+  new URL("../packages/data/data.json", import.meta.url),
+  JSON.stringify(compat, undefined, 2),
+);
